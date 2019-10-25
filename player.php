162eb5778fdc992417107f03ae09118715ac3461
Gaspar <sg506w@intl.att.com>
2019-10-25 12:47:35 +0200

    acas

--- player.php
+++ player.php
@@ -8,19 +8,38 @@ class Player
     {
     	$stderr =  fopen('php://stderr', 'w');
     	$my_cards = $game_state['players'][$game_state['in_action']]['hole_cards'];
+    	
+		foreach ($game_state['community_cards'] as $key => $value) {
+    		if ($my_cards[0]['rank'] == $value['rank'] || $my_cards[1]['rank'] == $value['rank']) {
+    			return 500;	
+    		}
+    	}
+
     	if($my_cards[0]['rank'] == $my_cards[1]['rank']) {
     		fwrite($stderr, "YAY! A pair\n");
         	return 700;
     	}
-    	
+        else {
+        	fwrite($stderr, "NAY! No pair\n");
+        	return 0;
+        }
+    }
+
+    public function showdown($game_state)
+    {
+    }
+}
+
+
+
 		// fwrite($stderr, "0" . $game_state['community_cards'][0]['rank']);
 
 
-		elseif ($my_cards[0]['rank'] == $game_state['community_cards'][0]['rank']) {
-			return 500;
-		}
+		// elseif ($my_cards[0]['rank'] == $game_state['community_cards'][0]['rank']) {
+		// 	return 500;
+		// }
 
-		
+
 		// elseif ($my_cards[0]['rank'] == $game_state['community_cards'][1]['rank']) {
 		// 	return 500;
 		// }
@@ -37,14 +56,3 @@ class Player
 		// 	fwrite($stderr, "NAY! No pair\n");
 		// 	return 500;
 		// }
-        else {
-        	fwrite($stderr, "NAY! No pair\n");
-        	return 0;
-        }
-    }
-
-    public function showdown($game_state)
-    {
-    }
-}
-
