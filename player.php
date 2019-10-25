118206f4909befb39d7a22432ab25271aa70ad37
Gaspar <sg506w@intl.att.com>
2019-10-25 10:52:18 +0200

    acaas

--- player.php
+++ player.php
@@ -6,7 +6,19 @@ class Player
 
     public function betRequest($game_state)
     {
-        return 0;
+    	$my_cards = $game_state['players'][$game_state['in_action']]['hole_cards'];
+    	if($my_cards[0]['rank'] == $my_cards[1]['rank']) {
+
+    		fwrite(STDERR, "YAY! A pair\n");
+        	return 10000;
+    	}
+    		
+        else {
+
+        	fwrite(STDERR, "NAY! No pair\n");
+        	return 0;
+        }
+        	
     }
 
     public function showdown($game_state)
