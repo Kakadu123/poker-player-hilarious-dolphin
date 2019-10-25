e42d437a747888dee004da7bf982b02376034512
Gaspar <sg506w@intl.att.com>
2019-10-25 12:54:59 +0200

    cascacsacasc

--- player.php
+++ player.php
@@ -9,25 +9,30 @@ class Player
     	$stderr =  fopen('php://stderr', 'w');
     	$my_cards = $game_state['players'][$game_state['in_action']]['hole_cards'];
     	
+		fwrite($stderr, " \r\n\r\n\n\n\n\n New Turnaj");
 
-		fwrite($stderr, "First" . $my_cards[0]['rank']);
-		fwrite($stderr, "Second" . $my_cards[1]['rank']);
+		fwrite($stderr, " \n First: " . $my_cards[0]['rank']);
+		fwrite($stderr, " \n Second: " . $my_cards[1]['rank']);
 		
 		foreach ($game_state['community_cards'] as $key => $value) {
     		fwrite($stderr, $value['rank']);
     		if ($my_cards[0]['rank'] == $value['rank'] || $my_cards[1]['rank'] == $value['rank']) {
-    			return 500;	
+    			return 1000;	
     		}
     	}
 
     	if($my_cards[0]['rank'] == $my_cards[1]['rank']) {
-    		fwrite($stderr, "YAY! A pair\n");
-        	return 700;
+    		fwrite($stderr, "\n YAY! A pair\n");
+        	return 1000;
     	}
         else {
         	fwrite($stderr, "NAY! No pair\n");
         	return 0;
         }
+
+		fwrite($stderr, " \n Koniec Turnaj \n\n\n\n\n");
+
+
     }
 
     public function showdown($game_state)
