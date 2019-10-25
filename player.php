399034bec1237047c5994c5cd8c011abe759d05d
Gaspar <sg506w@intl.att.com>
2019-10-25 13:53:55 +0200

    ccascasc

--- player.php
+++ player.php
@@ -16,21 +16,29 @@ class Player
 
 
 		 // fwrite($stderr, " \n Print Array Samo " . var_export($game_state));
+
+		// foreach ($game_state['community_cards'] as $value) {
+  //   		fwrite($stderr, " \n CommunityCard: " . $value['rank']);
+  //   		if ($my_cards[0]['rank'] == $value['rank'] || $my_cards[1]['rank'] == $value['rank']) {
+  //   			return 600;	
+  //   		}
+  //   	}
+
     	
 		foreach ($game_state['community_cards'] as $value) {
     		fwrite($stderr, " \n CommunityCard: " . $value['rank']);
     		if ($my_cards[0]['rank'] == $value['rank'] || $my_cards[1]['rank'] == $value['rank']) {
-    			return 10000;	
+    			return 600;	
     		}
     	}
 
     	if($my_cards[0]['rank'] == $my_cards[1]['rank']) {
     		fwrite($stderr, "\r\n YAY! A pair\n");
-        	return 10000;
+        	return 600;
     	}
         else {
         	fwrite($stderr, "\r\n NAY! No pair\n");
-        	return 0;
+        	return 100;
         }
 
 		fwrite($stderr, " \n Koniec Turnaj \n\n\n\n\n");
